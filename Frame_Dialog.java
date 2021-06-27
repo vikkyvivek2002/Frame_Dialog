58f7d2fa0dbbfba6d0f7f663147b105ad0cab1df
vikkyvivek2002 <84318895+vikkyvivek2002@users.noreply.github.com>
2021-06-27 17:48:07 +0530

    Add files via upload

---
+++ Frame_Dialog.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Frame_Dialog
+ {
+  String imputString =JOptionPane.showInputDialog("??");
+     int imput = Integer.parseInt(imputString);
+     imput++;
+	
+   }
+}
